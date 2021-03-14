d8b3cbfa6c82f312d0fca29e3cc06d1c6d3bbe56
Sean Jones <sean.d.jones92@gmail.com>
2021-03-14 15:14:39 -0600

    first attempt at functions

---
+++ functions/webhook/webhook.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+)
+
+func handler(request events.APIGatewayProxyRequest) (*events.APIGatewayProxyResponse, error) {
+	body := request.Body
+	fmt.Println(body)
+	return &events.APIGatewayProxyResponse{
+		StatusCode:      204,
+		IsBase64Encoded: false,
+	}, nil
+}
+
+func main() {
+	// Make the handler available for Remote Procedure Call by AWS Lambda
+	lambda.Start(handler)
+}
